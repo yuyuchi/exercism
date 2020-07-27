cd51efdd0a51115bf09fe477695afbab1d388bc9
Yuyu <viviyu1029@gmail.com>
2020-07-27 22:51:44 +0800

    simplified armstrong-numbers

--- armstrong-numbers/armstrong-numbers.js
+++ armstrong-numbers/armstrong-numbers.js
@@ -1,10 +1,10 @@
+const reducer = (acc, cur, _idx, arr) => {
+  return acc + Math.pow(cur, arr.length)
+}
+
 export const isArmstrongNumber = (num) => {
-  const digits = num.toString().length
-  let sum = 0
+  const splitNum = [...`${num}`]
+  const sum = splitNum.reduce(reducer, 0)
 
-  for (let i = 0; i < digits; i++) {
-    sum += Math.pow(num.toString().charAt(i), digits)
-  }
-
-  return sum === num ? true : false
+  return sum === num
 }

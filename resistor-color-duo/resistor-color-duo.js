af9a3a3d12fe3faa39e930986665f852a76e0eb1
Yuyu <viviyu1029@gmail.com>
2020-07-27 16:08:05 +0800

    using a helper method to make it individually

--- resistor-color-duo/resistor-color-duo.js
+++ resistor-color-duo/resistor-color-duo.js
@@ -1,26 +1,7 @@
-//
-// This is only a SKELETON file for the 'Resistor Color Duo' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-const RESISTOR_COLORS = [
-  'black',
-  'brown',
-  'red',
-  'orange',
-  'yellow',
-  'green',
-  'blue',
-  'violet',
-  'grey',
-  'white'
-]
+const COLORS = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white']
 
-export const decodedValue = (colorBands) => {
-  const selectedColors = colorBands.slice(0, 2)
+const colorCode = (color) => COLORS.indexOf(color)
 
-  const value = selectedColors
-    .map(color => RESISTOR_COLORS.indexOf(color))
-    .join('')
-  
-  return Number(value)
+export const decodedValue = (colors) => {
+  return colorCode(colors[0]) * 10 + colorCode(colors[1])
 }

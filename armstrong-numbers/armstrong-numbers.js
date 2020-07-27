3a959acc48aac66547388499f1f740ab19674ffa
Yuyu <viviyu1029@gmail.com>
2020-07-27 22:17:53 +0800

    simplified armstrong-numbers

--- armstrong-numbers/armstrong-numbers.js
+++ armstrong-numbers/armstrong-numbers.js
@@ -1,17 +1,10 @@
-//
-// This is only a SKELETON file for the 'Armstrong Numbers' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 export const isArmstrongNumber = (num) => {
   const digits = num.toString().length
-  var sum = 0
+  let sum = 0
 
-  for (var i = 0; i < digits; i++) {
-    sum = sum + Math.pow(num.toString().charAt(i), digits)
+  for (let i = 0; i < digits; i++) {
+    sum += Math.pow(num.toString().charAt(i), digits)
   }
-  if (sum !== num) {
-    return false
-  }
-  return true
-};
+
+  return sum === num ? true : false
+}

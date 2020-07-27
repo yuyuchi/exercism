675b8a5279af1c0e3ec42d91f1ef195f0bda80e0
Yuyu <viviyu1029@gmail.com>
2020-07-27 23:04:41 +0800

    darts

--- darts/darts.js
+++ darts/darts.js
@@ -1,24 +1,13 @@
-//
-// This is only a SKELETON file for the 'Darts' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
-const caculateHypotenuse = (a, b) => Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2))
+const calHypotenuse = (a, b) => Math.sqrt(a**2 + b**2)
 
 export const score = (x, y) => {
-  const a = Math.abs(x)
-  const b = Math.abs(y)
+  const h = calHypotenuse(x, y)
 
-  const c = caculateHypotenuse(a, b)
-
-  if (c <= 1) {
-    // inner circle
+  if (h <= 1) {
     return 10
-  } else if ( c > 1 && c <= 5) {
-    // middle circle
+  } else if (h <= 5) {
     return 5
-  } else if ( c > 5 && c <= 10) {
-    // outer circle
+  } else if (h <= 10) {
     return 1
   } else {
     return 0

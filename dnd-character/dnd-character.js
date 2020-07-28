7202e5d7909cd1fc79dbfdf02a2b3eb10ad1f11b
Yuyu <viviyu1029@gmail.com>
2020-07-28 18:15:43 +0800

    simplified dnd-character

--- dnd-character/dnd-character.js
+++ dnd-character/dnd-character.js
@@ -1,67 +1,63 @@
 export const abilityModifier = (sum) => {
-  if (sum < 3) {
-    throw new Error('Ability scores must be at least 3')
-  }
-  if (sum > 18) {
-    throw new Error('Ability scores can be at most 18')
-  }
+  if (sum < 3) throw new Error('Ability scores must be at least 3')
+  if (sum > 18) throw new Error('Ability scores can be at most 18')
+
   return Math.floor((sum - 10) / 2)
-};
+}
 
 const getRndInteger = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
-const reducer = (acc, val) => acc + val
-
 const getDiceSum = (min, max, diceNum) => {
-  let dices = []
+  let dices = [...new Array(diceNum)].map(() => getRndInteger(min, max))
 
-  for (let i = 0; i < diceNum; i++) {
-    dices = [...dices, getRndInteger(min, max)]
-  }
-  
-  const theLargestThree = dices.sort((a, b) => b - a).splice(0, 3)
-  return theLargestThree.reduce(reducer)
+  dices
+    .sort((a, b) => b - a)
+    .pop()
+    
+  return dices.reduce((a, b) => a + b, 0)
 }
 
 export class Character {
-  strength = getDiceSum(1, 6, 4)
-  dexterity = getDiceSum(1, 6, 4)
-  constitution = getDiceSum(1, 6, 4)
-  intelligence = getDiceSum(1, 6, 4)
-  wisdom = getDiceSum(1, 6, 4)
-  charisma = getDiceSum(1, 6, 4)
-
+  constructor() {
+    this._strength = Character.rollAbility()
+    this._dexterity = Character.rollAbility()
+    this._constitution = Character.rollAbility()
+    this._intelligence = Character.rollAbility()
+    this._wisdom = Character.rollAbility()
+    this._charisma = Character.rollAbility()
+  }
+  
   static rollAbility() {
     return getDiceSum(1, 6, 4)
   }
 
   get strength() {
-    return this.strength
+    return this._strength
   }
 
   get dexterity() {
-    return this.dexterity
+    return this._dexterity
   }
 
   get constitution() {
-    return this.constitution
+    return this._constitution
   }
 
   get intelligence() {
-    return this.intelligence
+    return this._intelligence
   }
 
   get wisdom() {
-    return this.wisdom
+    return this._wisdom
   }
 
   get charisma() {
-    return this.charisma
+    return this._charisma
   }
 
   get hitpoints() {
-    return 10 + abilityModifier(this.constitution)
+    return 10 + abilityModifier(this._constitution)
   }
 }

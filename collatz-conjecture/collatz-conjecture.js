cd1b3731081903943adc4a59ed472b84e1523442
Yuyu <viviyu1029@gmail.com>
2020-07-29 11:19:35 +0800

    use recursion instead of iteration

--- collatz-conjecture/collatz-conjecture.js
+++ collatz-conjecture/collatz-conjecture.js
@@ -1,15 +1,6 @@
-export const steps = (input) => {
-  if (input < 1) throw new Error('Only positive numbers are allowed')
+export const steps = (input, step = 0) => {
+  if (input <= 0) throw new Error('Only positive numbers are allowed')
+  if (input === 1) return step
   
-  let steps = 0
-  let num = input
-  while(num !== 1) {
-    if (num % 2 === 0) {
-      num /= 2
-    } else {
-      num = num * 3 + 1
-    }
-    steps++
-  }
-  return steps
+  return steps(input % 2 === 0 ? input / 2 : input * 3 + 1, step + 1)
 }

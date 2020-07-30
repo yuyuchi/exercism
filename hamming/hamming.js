cbf1b0ef5d36b62868af9ba925212c399e02e631
Yuyu <viviyu1029@gmail.com>
2020-07-30 15:17:19 +0800

    simplify hamming

--- hamming/hamming.js
+++ hamming/hamming.js
@@ -1,8 +1,8 @@
-export const compute = (strand1, strand2) => {  
-  if (strand1.length !== strand2.length) {
-    if (!strand1.length) throw new Error('left strand must not be empty')
-    if (!strand2.length) throw new Error('right strand must not be empty')
+export const compute = (a, b) => {  
+  if (a.length !== b.length) {
+    if (!a.length) throw new Error('left strand must not be empty')
+    if (!b.length) throw new Error('right strand must not be empty')
     throw new Error('left and right strands must be of equal length')
   }
-  return [...strand1].reduce((acc, val, idx) => acc + (val !== [...strand2][idx]), 0)
+  return [...a].reduce((acc, val, idx) => acc + (val !== [...b][idx]), 0)
 }

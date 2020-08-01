68e770c78e55cb8305fa3b84a081f1429ffd1cb2
Yuyu <viviyu1029@gmail.com>
2020-08-01 22:40:15 +0800

    extract share function

--- clock/clock.js
+++ clock/clock.js
@@ -3,21 +3,14 @@ export class Clock {
     this._hours = hours
     this._minutes = minutes
   }
-  
-  padZero(num) {
-    return num.toString().padStart(2, '0')
-  }
 
   toString() {
     this._hours += Math.floor(this._minutes / 60)
     
-    this._hours %= 24    
-    this._minutes %= 60
+    this._hours = this._roundClock(this._hours, 24)
+    this._minutes = this._roundClock(this._minutes, 60)
 
-    if (this._hours < 0) this._hours += 24
-    if (this._minutes < 0) this._minutes += 60
-
-    return `${this.padZero(this._hours)}:${this.padZero(this._minutes)}`
+    return `${this._padZero(this._hours)}:${this._padZero(this._minutes)}`
   }
 
   plus(minutes) {
@@ -32,5 +25,14 @@ export class Clock {
 
   equals(clock) {
     return this.toString() === clock.toString()
+  }
+
+  _padZero(num) {
+    return num.toString().padStart(2, '0')
+  }
+
+  _roundClock(num, unit) {
+    const correction = num % unit
+    return correction >= 0 ? correction : correction + unit
   }
 }

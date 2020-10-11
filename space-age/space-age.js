51032dcc3e8a012875709a85c6b8f0c7712cdeb9
Yuyu <viviyu1029@gmail.com>
2020-10-11 18:15:26 +0800

    readability improvements

--- space-age/space-age.js
+++ space-age/space-age.js
@@ -1,4 +1,4 @@
-const ORBITAL_PERIOD = {
+const ORBITAL_PERIODS_PER_EARTH_YEAR = {
   mercury: 0.2408467,
   venus: 0.61519726,
   earth: 1,
@@ -8,6 +8,11 @@ const ORBITAL_PERIOD = {
   uranus: 84.016846,
   neptune: 164.79132
 }
+const SECONDS_PER_EARTH_YEAR = 31557600
+
 export const age = (planet, sec) => {
-  return +(sec / 31557600 / ORBITAL_PERIOD[planet]).toFixed(2)
+  const earthYears = sec / SECONDS_PER_EARTH_YEAR
+  const orbitalPeriods = earthYears / ORBITAL_PERIODS_PER_EARTH_YEAR[planet]
+
+  return Math.round(orbitalPeriods * 100) / 100
 }

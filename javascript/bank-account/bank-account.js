0f69bfb4c837261edc0f9225e93740667896eba3
Yuyu <yuchi@xfers.com>
2022-08-31 22:06:20 +0800

    feat: 🎸 Provide meaningful error message

--- javascript/bank-account/bank-account.js
+++ javascript/bank-account/bank-account.js
@@ -10,30 +10,31 @@ export class BankAccount {
   }
 
   open() {
-    if (!this._isAccountClosed) throw new ValueError();
+    if (!this._isAccountClosed) throw new ValueError("Account is already open");
     this._amount = 0;
     this._isAccountClosed = false;
   }
 
   close() {
-    if (this._isAccountClosed) throw new ValueError();
+    if (this._isAccountClosed) throw new ValueError("Account is not open");
     this._isAccountClosed = true;
   }
 
   deposit(currency) {
-    if (this._isAccountClosed) throw new ValueError();
-    if (currency < 0) throw new ValueError();
+    if (this._isAccountClosed) throw new ValueError("Account is not open");
+    if (currency < 0) throw new ValueError("Amount cannot be negative");
     this._amount += currency;
   }
 
   withdraw(currency) {
-    if (this._isAccountClosed) throw new ValueError();
-    if (currency > this._amount || currency < 0) throw new ValueError();
+    if (this._isAccountClosed) throw new ValueError("Account is not open");
+    if (currency > this._amount) throw new ValueError("Balance is too low");
+    if (currency < 0) throw new ValueError("Amount cannot be negative");
     this._amount -= currency;
   }
 
   get balance() {
-    if (this._isAccountClosed) throw new ValueError();
+    if (this._isAccountClosed) throw new ValueError("Account is not open");
     return this._amount;
   }
 }
